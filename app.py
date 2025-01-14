51c23d81d9c69c01f3162500e80fd55dc3e24f3b
Antti Kivi <antti@anttikivi.fi>
2025-01-14 23:47:32 +0200

    feat: change the site name to Finnish

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-context = {"site": {"subtitle": "Kirjat purkissa", "title": "Flask Library"}}
+context = {"site": {"subtitle": "Kirjat purkissa", "title": "Flask-kirjasto"}}
 
 
 @app.route("/")
