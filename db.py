bee281dc047d0509fce9bc600401e8eab21d2ed4
Antti Kivi <antti@anttikivi.fi>
2025-01-15 20:14:16 +0200

    feat: add helper function for executing database modifications

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
-from collections.abc import Sequence
 import sqlite3
+from collections.abc import Sequence
 from typing import Any
+
+from flask import g
 
 
 def get_connection() -> sqlite3.Connection:
@@ -8,6 +10,20 @@ def get_connection() -> sqlite3.Connection:
     _ = con.execute("PRAGMA foreign_keys = ON")
     con.row_factory = sqlite3.Row
     return con
+
+
+def execute(sql: str, params: Sequence[Any] | None = None) -> None:  # pyright: ignore[reportExplicitAny]
+    if params is None:
+        params = []
+    con = get_connection()
+    result = con.execute(sql, params)
+    con.commit()
+    g.last_insert_id = result.lastrowid
+    con.close()
+
+
+def last_insert_id() -> Any:  # pyright: ignore[reportAny,reportExplicitAny]
+    return g.last_insert_id  # pyright: ignore[reportAny]
 
 
 def query(sql: str, params: Sequence[Any] | None = None) -> Sequence[Any]:  # pyright: ignore[reportExplicitAny]
