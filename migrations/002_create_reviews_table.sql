340824dbc5d708f8f888cfec2c270c6c4f53d610
Antti Kivi <antti@anttikivi.fi>
2025-02-16 16:18:52 +0200

    feat: add time to the reviews table

--- migrations/002_create_reviews_table.sql
+++ migrations/002_create_reviews_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS reviews (
   book_id INTEGER NOT NULL,
   stars INTEGER NOT NULL,
   message TEXT,
+  time TEXT,
   FOREIGN KEY(user_id) REFERENCES users(id),
   FOREIGN KEY(book_id) REFERENCES books(id)
 )
