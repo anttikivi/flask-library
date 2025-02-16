29a75a62cefc58f1bcfde93d8ad3c43d9d66c72e
Antti Kivi <antti@anttikivi.fi>
2025-02-16 15:59:19 +0200

    feat: create migration for adding the reviews table

---
+++ migrations/002_create_reviews_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS reviews (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  book_id INTEGER NOT NULL,
+  stars INTEGER NOT NULL,
+  message TEXT,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(book_id) REFERENCES books(id)
+)
