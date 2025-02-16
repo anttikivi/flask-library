a2397aa547fb17de23824da426ee617406566d1f
Antti Kivi <antti@anttikivi.fi>
2025-02-16 13:17:31 +0200

    feat: add migration to create a table for read books

---
+++ migrations/001_create_read_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS read_books (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  book_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(book_id) REFERENCES books(id)
+)
