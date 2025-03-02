454fbb775e36ad5832d0c72eacea0c42b6881894
Antti Kivi <antti@anttikivi.fi>
2025-03-02 19:24:25 +0200

    test: add test data for trying the app with a huge number of authors

--- seed.py
+++ seed.py
@@ -2,19 +2,17 @@
 
 import sqlite3
 
+USER_COUNT = 10000
+AUTHOR_COUNT = 10**6
+
 if __name__ == "__main__":
     db = sqlite3.connect("database.db")
 
     _ = db.execute("DELETE FROM users")
 
-    user_count = 10000
-
-    sql = "INSERT INTO users (username, password_hash) VALUES "
-    values = ["(?, ?)"] * user_count
-    sql += ", ".join(values)
     params: list[str] = []
 
-    for i in range(1, user_count + 1):
+    for i in range(1, USER_COUNT + 1):
         params.append("user" + str(i))
 
         # NOTE: It is stupid to store the passwords in plain text, but
@@ -23,7 +21,23 @@ if __name__ == "__main__":
         # are computed.
         params.append("pwd" + str(i))
 
+    sql = "INSERT INTO users (username, password_hash) VALUES "
+    values = ["(?, ?)"] * USER_COUNT
+    sql += ", ".join(values)
+
     _ = db.execute(sql, params)
+
+    authors: list[tuple[str, str]] = []
+
+    for i in range(1, AUTHOR_COUNT + 1):
+        authors.append(("first_name_" + str(i), "surname_" + str(i)))
+
+    _ = db.execute("DELETE FROM authors")
+
+    sql = "INSERT INTO authors (first_name, surname) VALUES (?, ?)"
+
+    for a in authors:
+        _ = db.execute(sql, [a[0], a[1]])
 
     db.commit()
     db.close()
